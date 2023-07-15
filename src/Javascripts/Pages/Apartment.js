2f56408ee94879fae1662b8fcd972c70f38e3255
AmandineUhl <didine709@hotmail.com>
2023-07-15 11:04:32 +0200

    apartment page

--- src/Javascripts/Pages/Apartment.js
+++ src/Javascripts/Pages/Apartment.js
@@ -4,7 +4,7 @@ function Apartment() {
   return (
     <div className="apartment">
       <div className="apartment_caroussel">
-        <img src="public/images/banner.jpg" alt="" />
+        <img src="./images/banner.jpg" alt="" />
       </div>
       <div className="apartment_presentation">
         <h1>Crazy loft on canal Saint martin</h1>
@@ -13,8 +13,27 @@ function Apartment() {
         <p>Canal</p>
         <p>Paris</p>
       </div>
+      <div className="apartment_tenant">
+        <h3>Alexandre Dumas</h3>
+        <div className="apartment_tenant_badge"></div>
+        <div className="apartment_tenant_stars">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="collapse">
+          <p>Description</p>
+          <p>fihez iohf eziohf oifehz fhezohfoh ez ezfh ohfezoi  ehfmhzhf ogrm egoi mj mhr gmh ermg g rmzgh h iorhg ozhùi</p>
+        </div>
+        <div className="collapse">
+          <p>Equipements</p>
+          <p>fihez iohf eziohf oifehz fhezohfoh ez ezfh ohfezoi  ehfmhzhf ogrm egoi mj mhr gmh ermg g rmzgh h iorhg ozhùi</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Apartment
+export default Apartment;
--- src/Javascripts/components/Card.jsx
+++ src/Javascripts/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Card() {
   return (
     <div>
       <div className="card">
-        <div className="card_title">Titre de la location</div>
+        <Link to="/apartment" className="card_title">Titre de la location</Link>
       </div>
     </div>
   );
