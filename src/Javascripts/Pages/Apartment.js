f0bf1cffabb55efc389a5f5bf79be2e103b4b43f
AmandineUhl <didine709@hotmail.com>
2023-07-15 10:34:23 +0200

    error page

--- src/Javascripts/Pages/Apartment.js
+++ src/Javascripts/Pages/Apartment.js
@@ -1,12 +1,18 @@
-import React from 'react'
-
-
-
+import React from "react";
 
 function Apartment() {
   return (
-    <div>
-    
+    <div className="apartment">
+      <div className="apartment_caroussel">
+        <img src="public/images/banner.jpg" alt="" />
+      </div>
+      <div className="apartment_presentation">
+        <h1>Crazy loft on canal Saint martin</h1>
+        <h2>Paris, ile de france</h2>
+        <p>Cozy</p>
+        <p>Canal</p>
+        <p>Paris</p>
+      </div>
     </div>
   )
 }
